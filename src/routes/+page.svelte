109112f6ce3bc25a681379bdad5576018c87112e
DreamTexX <kevin.cebulla@dreamtexx.email>
2023-08-31 15:26:46 +0000

    chore: add simple custom welcome screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a>
-     to read the documentation
-</p>
+<div class="flex h-screen flex-col items-center justify-center gap-2">
+    <div class="flex items-center gap-4">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="0.75"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-hand-metal"
+        >
+            <path d="M18 12.5V10a2 2 0 0 0-2-2v0a2 2 0 0 0-2 2v1.4" />
+            <path d="M14 11V9a2 2 0 1 0-4 0v2" />
+            <path d="M10 10.5V5a2 2 0 1 0-4 0v9" />
+            <path
+                d="m7 15-1.76-1.76a2 2 0 0 0-2.83 2.82l3.6 3.6C7.5 21.14 9.2 22 12 22h2a8 8 0 0 0 8-8V7a2 2 0 1 0-4 0v5"
+            />
+        </svg>
+        <h1 class="text-xl font-semibold">Welcome to SvelteKit</h1>
+    </div>
+    <p>
+        Visit <a href="https://kit.svelte.dev" class="text-blue-500">kit.svelte.dev</a>
+        to read the documentation
+    </p>
+</div>
